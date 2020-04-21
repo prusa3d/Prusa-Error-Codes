b8061ba46e6f238428eb8f6ed8bb0b10975655a5
Vladimír Matěna <vlada.matena@gmail.com>
2020-04-21 17:33:00 +0200

    Add LICENSE and setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# This file is part of the SL1 firmware
+# Copyright (C) 2020 Prusa Research a.s. - www.prusa3d.com
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from setuptools import setup, find_packages
+
+setup(
+    name="sl1-errors",
+    version="2020.4.21",
+    packages=find_packages(exclude=["tests"]),
+    url="https://gitlab.com/prusa3d/sl1/sl1-errors",
+    license="GNU General Public License v3 or later (GPLv3+)",
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
+    ]
+)
