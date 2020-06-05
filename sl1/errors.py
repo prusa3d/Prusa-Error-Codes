b8c3ca832b15d0ca02e9d23e914f87fd2dff68fd
Vladimír Matěna <vlada.matena@gmail.com>
2020-06-05 13:37:42 +0200

    Sort sl1 errors by category

--- sl1/errors.py
+++ sl1/errors.py
@@ -20,15 +20,36 @@ class Sl1Codes(Codes):
 
     PRINTER = Printer.SL1
 
-    # Basic error codes
-    NONE = Code(PRINTER, Category.SYSTEM, 0, "No problem")
-    UNKNOWN = Code(PRINTER, Category.SYSTEM, 1, "An unknown error has occurred")
-
-    # General error codes
+    # Mechanical
     GENERAL_TILT_HOME_FAILURE = Code(PRINTER, Category.MECHANICAL, 1, None)
     GENERAL_TOWER_HOME_FAILURE = Code(PRINTER, Category.MECHANICAL, 2, None)
-    GENERAL_CONFIG_EXCEPTION = Code(PRINTER, Category.SYSTEM, 5, "Failed to read configuration file")
+    EXPOSURE_TOWER_MOVE_FAILURE = Code(PRINTER, Category.MECHANICAL, 3, None)
+    EXPOSURE_FAN_FAILURE = Code(PRINTER, Category.MECHANICAL, 6, None)
+    EXPOSURE_RESIN_TOO_LOW = Code(PRINTER, Category.MECHANICAL, 8, None)
+    EXPOSURE_RESIN_TOO_HIGH = Code(PRINTER, Category.MECHANICAL, 9, None)
+    EXPOSURE_TILT_FAILURE = Code(PRINTER, Category.MECHANICAL, 10, None)
+    EXPOSURE_TOWER_FAILURE = Code(PRINTER, Category.MECHANICAL, 12, None)
+    GENERAL_NOT_MECHANICALLY_CALIBRATED = Code(PRINTER, Category.MECHANICAL, 13, None)
+
+    # Temperature
+    EXPOSURE_TEMP_SENSOR_FAILURE = Code(PRINTER, Category.TEMPERATURE, 5, None)
+
+    # Connectivity
+    GENERAL_FAILED_TO_MQTT_SEND = Code(PRINTER, Category.CONNECTIVITY, 1, "Cannot send factory config to MQTT!")
+    GENERAL_NOT_CONNECTED_TO_NETWORK = Code(PRINTER, Category.CONNECTIVITY, 2, None)
+    GENERAL_CONNECTION_FAILED = Code(PRINTER, Category.CONNECTIVITY, 3, None)
+    GENERAL_DOWNLOAD_FAILED = Code(PRINTER, Category.CONNECTIVITY, 4, None)
+
+    # Electrical
     GENERAL_MOTION_CONTROLLER_EXCEPTION = Code(PRINTER, Category.ELECTRICAL, 6, None)
+    EXPOSURE_RESIN_SENSOR_FAILURE = Code(PRINTER, Category.ELECTRICAL, 7, None)
+    GENERAL_NOT_UV_CALIBRATED = Code(PRINTER, Category.ELECTRICAL, 8, None)
+
+    # System
+    NONE = Code(PRINTER, Category.SYSTEM, 0, "No problem")
+    UNKNOWN = Code(PRINTER, Category.SYSTEM, 1, "An unknown error has occurred")
+    EXPOSURE_PROJECT_FAILURE = Code(PRINTER, Category.SYSTEM, 4, None)
+    GENERAL_CONFIG_EXCEPTION = Code(PRINTER, Category.SYSTEM, 5, "Failed to read configuration file")
     GENERAL_NOT_AVAILABLE_IN_STATE = Code(PRINTER, Category.SYSTEM, 6, None)
     GENERAL_DBUS_MAPPING_EXCEPTION = Code(PRINTER, Category.SYSTEM, 7, None)
     GENERAL_REPRINT_WITHOUT_HISTORY = Code(PRINTER, Category.SYSTEM, 8, None)
@@ -40,32 +61,15 @@ class Sl1Codes(Codes):
         PRINTER, Category.SYSTEM, 11, "The automatic UV LED calibration did not finish successfully!"
     )
     GENERAL_MISSING_UVPWM_SETTINGS = Code(PRINTER, Category.SYSTEM, 12, None)
-    GENERAL_FAILED_TO_MQTT_SEND = Code(PRINTER, Category.CONNECTIVITY, 1, "Cannot send factory config to MQTT!")
     GENERAL_FAILED_UPDATE_CHANNEL_SET = Code(PRINTER, Category.SYSTEM, 13, "Cannot set update channel")
     GENERAL_FAILED_UPDATE_CHANNEL_GET = Code(PRINTER, Category.SYSTEM, 14, None)
-    GENERAL_NOT_CONNECTED_TO_NETWORK = Code(PRINTER, Category.CONNECTIVITY, 2, None)
-    GENERAL_CONNECTION_FAILED = Code(PRINTER, Category.CONNECTIVITY, 3, None)
-    GENERAL_DOWNLOAD_FAILED = Code(PRINTER, Category.CONNECTIVITY, 4, None)
+    EXPOSURE_WARNING_ESCALATION = Code(PRINTER, Category.SYSTEM, 15, None)
     GENERAL_NOT_ENOUGH_INTERNAL_SPACE = Code(PRINTER, Category.SYSTEM, 16, None)
     GENERAL_ADMIN_NOT_AVAILABLE = Code(PRINTER, Category.SYSTEM, 17, None)
     GENERAL_FILE_NOT_FOUND = Code(PRINTER, Category.SYSTEM, 18, "Cannot find a file!")
     GENERAL_INVALID_EXTENSION = Code(PRINTER, Category.SYSTEM, 19, "File has an invalid extension!")
     GENERAL_FILE_ALREADY_EXISTS = Code(PRINTER, Category.SYSTEM, 20, "File already exists!")
     GENERAL_INVALID_PROJECT = Code(PRINTER, Category.SYSTEM, 21, "The project file is invalid!")
-    GENERAL_NOT_MECHANICALLY_CALIBRATED = Code(PRINTER, Category.MECHANICAL, 13, None)
-    GENERAL_NOT_UV_CALIBRATED = Code(PRINTER, Category.ELECTRICAL, 8, None)
-
-    # Exposure error codes
-    EXPOSURE_TILT_FAILURE = Code(PRINTER, Category.MECHANICAL, 10, None)
-    EXPOSURE_TOWER_FAILURE = Code(PRINTER, Category.MECHANICAL, 12, None)
-    EXPOSURE_TOWER_MOVE_FAILURE = Code(PRINTER, Category.MECHANICAL, 3, None)
-    EXPOSURE_PROJECT_FAILURE = Code(PRINTER, Category.SYSTEM, 4, None)
-    EXPOSURE_TEMP_SENSOR_FAILURE = Code(PRINTER, Category.TEMPERATURE, 5, None)
-    EXPOSURE_FAN_FAILURE = Code(PRINTER, Category.MECHANICAL, 6, None)
-    EXPOSURE_RESIN_SENSOR_FAILURE = Code(PRINTER, Category.ELECTRICAL, 7, None)
-    EXPOSURE_RESIN_TOO_LOW = Code(PRINTER, Category.MECHANICAL, 8, None)
-    EXPOSURE_RESIN_TOO_HIGH = Code(PRINTER, Category.MECHANICAL, 9, None)
-    EXPOSURE_WARNING_ESCALATION = Code(PRINTER, Category.SYSTEM, 15, None)
 
     @classmethod
     def get(cls, code: str):
