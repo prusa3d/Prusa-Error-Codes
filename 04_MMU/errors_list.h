7bcbaa22061daaaafb61dc8cb67c1f168280964c
D.R.racer <drracer@drracer.eu>
2022-10-26 11:15:50 +0200

    Add MMU Selftest Failed + update errors_list.h

--- 04_MMU/errors_list.h
+++ 04_MMU/errors_list.h
@@ -4,6 +4,7 @@
 #pragma once
 #include "inttypes.h"
 #include "i18n.h"
+#include "button_operations.h"
 
 namespace MMU2 {
 
@@ -14,26 +15,27 @@ typedef enum : uint16_t {
 
     ERR_MECHANICAL = 100,
     ERR_MECHANICAL_FINDA_DIDNT_TRIGGER,
-    ERR_MECHANICAL_FINDA_DIDNT_SWITCH_OFF,
+    ERR_MECHANICAL_FINDA_FILAM_STUCK,
     ERR_MECHANICAL_FSENSOR_DIDNT_TRIGGER,
-    ERR_MECHANICAL_FSENSOR_DIDNT_SWITCH_OFF,
+    ERR_MECHANICAL_FSENSOR_FILAM_STUCK,
 
-    ERR_MECHANICAL_PULLEY_STALLED = 105,
-    ERR_MECHANICAL_FSENSOR_TOO_EARLY = 106,
-    
+    ERR_MECHANICAL_PULLEY_CANNOT_MOVE = 105,
     ERR_MECHANICAL_SELECTOR_CANNOT_HOME = 115,
     ERR_MECHANICAL_SELECTOR_CANNOT_MOVE = 116,
     ERR_MECHANICAL_IDLER_CANNOT_HOME = 125,
     ERR_MECHANICAL_IDLER_CANNOT_MOVE = 126,
 
-    ERR_TEMPERATURE = 200,
-    ERR_TEMPERATURE_TMC_PULLEY_OVER_TEMPERATURE_WARN = 201,
-    ERR_TEMPERATURE_TMC_SELECTOR_OVER_TEMPERATURE_WARN = 211,
-    ERR_TEMPERATURE_TMC_IDLER_OVER_TEMPERATURE_WARN = 221,
+    ERR_MECHANICAL_FSENSOR_TOO_EARLY = 106,
 
-    ERR_TEMPERATURE_TMC_PULLEY_OVER_TEMPERATURE_ERROR = 202,
-    ERR_TEMPERATURE_TMC_SELECTOR_OVER_TEMPERATURE_ERROR = 212,
-    ERR_TEMPERATURE_TMC_IDLER_OVER_TEMPERATURE_ERROR = 222,
+
+    ERR_TEMPERATURE = 200,
+    ERR_TEMPERATURE_TMC_PULLEY_WARNING_TMC_TOO_HOT = 201,
+    ERR_TEMPERATURE_TMC_SELECTOR_WARNING_TMC_TOO_HOT = 211,
+    ERR_TEMPERATURE_TMC_IDLER_WARNING_TMC_TOO_HOT = 221,
+
+    ERR_TEMPERATURE_TMC_PULLEY_TMC_OVERHEAT_ERROR = 202,
+    ERR_TEMPERATURE_TMC_SELECTOR_TMC_OVERHEAT_ERROR = 212,
+    ERR_TEMPERATURE_TMC_IDLER_TMC_OVERHEAT_ERROR = 222,
 
 
     ERR_ELECTRICAL = 300,
@@ -52,6 +54,10 @@ typedef enum : uint16_t {
     ERR_ELECTRICAL_TMC_PULLEY_DRIVER_SHORTED = 304,
     ERR_ELECTRICAL_TMC_SELECTOR_DRIVER_SHORTED = 314,
     ERR_ELECTRICAL_TMC_IDLER_DRIVER_SHORTED = 324,
+    
+    ERR_ELECTRICAL_PULLEY_SELFTEST_FAILED = 305,
+    ERR_ELECTRICAL_SELECTOR_SELFTEST_FAILED = 315,
+    ERR_ELECTRICAL_IDLER_SELFTEST_FAILED = 325,
 
 
     ERR_CONNECT = 400,
@@ -63,25 +69,12 @@ typedef enum : uint16_t {
     ERR_SYSTEM_FILAMENT_ALREADY_LOADED = 501,
     ERR_SYSTEM_INVALID_TOOL = 502,
     ERR_SYSTEM_QUEUE_FULL = 503,
-    ERR_SYSTEM_VERSION_MISMATCH = 504,
-    ERR_SYSTEM_RUNTIME_ERROR = 505,
-    ERR_SYSTEM_FINDA_VS_EEPROM_DISREPANCY = 506,
+    ERR_SYSTEM_FW_UPDATE_NEEDED = 504,
+    ERR_SYSTEM_FW_RUNTIME_ERROR = 505,
+    ERR_SYSTEM_UNLOAD_MANUALLY = 506,
 
     ERR_OTHER = 900
 } err_num_t;
-
-/// Will be mapped onto dialog button responses in the Buddy FW
-/// Those responses have their unique+translated texts as well
-enum class ButtonOperations : uint8_t {
-    NoOperation,
-    Retry,
-    SlowLoad,
-    Continue,
-    ResetMMU,
-    Unload,
-    StopPrint,
-    DisableMMU,
-};
 
 struct MMUErrorDesc {
     // 32 bit
@@ -99,40 +92,40 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("FINDA DIDNT TRIGGER"),
         // r=5, c=20
-        N_("FINDA didn't trigger while loading filament. Ensure filament can move and FINDA works."),
+        N_("FINDA didn't trigger while loading the filament. Ensure the filament can move and FINDA works."),
         ERR_MECHANICAL_FINDA_DIDNT_TRIGGER,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
-    { N_("FINDA DIDNT GO OFF"),
+    { N_("FINDA: FILAM. STUCK"),
         // r=5, c=20
         N_("FINDA didn't switch off while unloading filament. Try unloading manually. Ensure filament can move and FINDA works."),
-        ERR_MECHANICAL_FINDA_DIDNT_SWITCH_OFF,
+        ERR_MECHANICAL_FINDA_FILAM_STUCK,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
     { N_("FSENSOR DIDNT TRIGGER"),
         // r=5, c=20
-        N_("Filament sensor didn't trigger while loading filament. Ensure filament reached the fsensor and the sensor works."),
+        N_("Filament sensor didn't trigger while loading the filament. Ensure the filament reached the fsensor and the sensor works."),
         ERR_MECHANICAL_FSENSOR_DIDNT_TRIGGER,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
-    { N_("FSENSOR DIDNT GO OFF"),
+    { N_("FSENSOR: FIL. STUCK"),
         // r=5, c=20
         N_("Filament sensor didn't switch off while unloading filament. Ensure filament can move and the sensor works."),
-        ERR_MECHANICAL_FSENSOR_DIDNT_SWITCH_OFF,
+        ERR_MECHANICAL_FSENSOR_FILAM_STUCK,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
     { N_("PULLEY CANNOT MOVE"),
         // r=5, c=20
-        N_("The Pulley motor stalled - Ensure the pulley can move and check the wiring."),
-        ERR_MECHANICAL_PULLEY_STALLED,
+        N_("Pulley motor stalled. Ensure the pulley can move and check the wiring."),
+        ERR_MECHANICAL_PULLEY_CANNOT_MOVE,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
@@ -147,7 +140,7 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("SELECTOR CANNOT HOME"),
         // r=5, c=20
-        N_("The Selector cannot home properly - check for anything blocking its movement."),
+        N_("The Selector cannot home properly. Check for anything blocking its movement."),
         ERR_MECHANICAL_SELECTOR_CANNOT_HOME,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
@@ -155,15 +148,15 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("SELECTOR CANNOT MOVE"),
         // r=5, c=20
-        N_("The Selector cannot move - check for anything blocking its movement. Check the wiring is correct."),
-        ERR_MECHANICAL_SELECTOR_CANNOT_HOME,
+        N_("The Selector cannot move. Check for anything blocking its movement. Check the wiring is correct."),
+        ERR_MECHANICAL_SELECTOR_CANNOT_MOVE,
         { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
     { N_("IDLER CANNOT HOME"),
         // r=5, c=20
-        N_("The Idler cannot home properly - check for anything blocking its movement."),
+        N_("The Idler cannot home properly. Check for anything blocking its movement."),
         ERR_MECHANICAL_IDLER_CANNOT_HOME,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
@@ -171,8 +164,8 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("IDLER CANNOT MOVE"),
         // r=5, c=20
-        N_("The Idler cannot move properly - check for anything blocking its movement. Check the wiring is correct."),
-        ERR_MECHANICAL_IDLER_CANNOT_HOME,
+        N_("The Idler cannot move properly. Check for anything blocking its movement. Check the wiring is correct."),
+        ERR_MECHANICAL_IDLER_CANNOT_MOVE,
       { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     },
 
@@ -183,7 +176,7 @@ static constexpr MMUErrorDesc error_list[] = {
     { N_("WARNING TMC TOO HOT"),
         // r=5, c=20
         N_("TMC driver for the Pulley motor is almost overheating. Make sure there is sufficient airflow near the MMU board."),
-        ERR_TEMPERATURE_TMC_PULLEY_OVER_TEMPERATURE_WARN,
+        ERR_TEMPERATURE_TMC_PULLEY_WARNING_TMC_TOO_HOT,
         { ButtonOperations::Continue, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
 
@@ -191,7 +184,7 @@ static constexpr MMUErrorDesc error_list[] = {
     { N_("WARNING TMC TOO HOT"),
         // r=5, c=20
         N_("TMC driver for the Selector motor is almost overheating. Make sure there is sufficient airflow near the MMU board."),
-        ERR_TEMPERATURE_TMC_SELECTOR_OVER_TEMPERATURE_WARN,
+        ERR_TEMPERATURE_TMC_SELECTOR_WARNING_TMC_TOO_HOT,
       { ButtonOperations::Continue, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
 
@@ -199,7 +192,7 @@ static constexpr MMUErrorDesc error_list[] = {
     { N_("WARNING TMC TOO HOT"),
         // r=5, c=20
         N_("TMC driver for the Idler motor is almost overheating. Make sure there is sufficient airflow near the MMU board."),
-        ERR_TEMPERATURE_TMC_IDLER_OVER_TEMPERATURE_WARN,
+        ERR_TEMPERATURE_TMC_IDLER_WARNING_TMC_TOO_HOT,
       { ButtonOperations::Continue, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
     
@@ -207,23 +200,23 @@ static constexpr MMUErrorDesc error_list[] = {
     { N_("TMC OVERHEAT ERROR"),
         // r=5, c=20
         N_("TMC driver for the Pulley motor is overheated. Cool down the MMU board and reset MMU."),
-        ERR_TEMPERATURE_TMC_PULLEY_OVER_TEMPERATURE_ERROR,
+        ERR_TEMPERATURE_TMC_PULLEY_TMC_OVERHEAT_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
     { N_("TMC OVERHEAT ERROR"),
         // r=5, c=20
-        N_("TMC driver for the Selector motor is overheated. Cool down the MMU board and restart MMU."),
-        ERR_TEMPERATURE_TMC_SELECTOR_OVER_TEMPERATURE_ERROR,
+        N_("TMC driver for the Selector motor is overheated. Cool down the MMU board and reset MMU."),
+        ERR_TEMPERATURE_TMC_SELECTOR_TMC_OVERHEAT_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
     { N_("TMC OVERHEAT ERROR"),
         // r=5, c=20
-        N_("TMC driver for the Idler motor is overheated. Cool down the MMU board and restart MMU."),
-        ERR_TEMPERATURE_TMC_IDLER_OVER_TEMPERATURE_ERROR,
+        N_("TMC driver for the Idler motor is overheated. Cool down the MMU board and reset MMU."),
+        ERR_TEMPERATURE_TMC_IDLER_TMC_OVERHEAT_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
     
@@ -233,7 +226,7 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("TMC DRIVER ERROR"),
         // r=5, c=20
-        N_("TMC driver for the Pulley motor is not responding. Try restarting the MMU. If the issue persists contact support."),
+        N_("TMC driver for the Pulley motor is not responding. Try resetting the MMU. If the issue persists contact support."),
         ERR_ELECTRICAL_TMC_PULLEY_DRIVER_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
@@ -241,7 +234,7 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("TMC DRIVER ERROR"),
         // r=5, c=20
-        N_("TMC driver for the Selector motor is not responding. Try restarting the MMU. If the issue persists contact support."),
+        N_("TMC driver for the Selector motor is not responding. Try resetting the MMU. If the issue persists contact support."),
         ERR_ELECTRICAL_TMC_SELECTOR_DRIVER_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
@@ -249,7 +242,7 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("TMC DRIVER ERROR"),
         // r=5, c=20
-        N_("TMC driver for the Idler motor is not responding. Try restarting the MMU. If the issue persists contact support."),
+        N_("TMC driver for the Idler motor is not responding. Try resetting the MMU. If the issue persists contact support."),
         ERR_ELECTRICAL_TMC_IDLER_DRIVER_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
@@ -326,13 +319,37 @@ static constexpr MMUErrorDesc error_list[] = {
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
 
+    // r=1, c=20
+    { N_("MMU SELFTEST FAILED"),
+        // r=5, c=20
+        N_("MMU selftest failed on the Pulley TMC driver. Check the wiring and connectors. If the issue persists contact support."),
+        ERR_ELECTRICAL_PULLEY_SELFTEST_FAILED,
+        { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
+    },
+    
+    // r=1, c=20
+    { N_("MMU SELFTEST FAILED"),
+        // r=5, c=20
+        N_("MMU selftest failed on the Selector TMC driver. Check the wiring and connectors. If the issue persists contact support."),
+        ERR_ELECTRICAL_SELECTOR_SELFTEST_FAILED,
+        { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
+    },
+    
+    // r=1, c=20
+    { N_("MMU SELFTEST FAILED"),
+        // r=5, c=20
+        N_("MMU selftest failed on the Idler TMC driver. Check the wiring and connectors. If the issue persists contact support."),
+        ERR_ELECTRICAL_IDLER_SELFTEST_FAILED,
+        { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
+    },
+
     /////////////////////////////////////////////////////////////////////////////////////////////////////
     // COMMUNICATION
     
     // r=1, c=20
     { N_("MMU NOT RESPONDING"),
         // r=5, c=20
-        N_("MMU unit not responding. Check the wiring and connectors. If the issue persists contact support."),
+        N_("MMU unit not responding. Check the wiring and connectors. If the issue persists, contact support."),
         ERR_CONNECT_MMU_NOT_RESPONDING,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
@@ -340,7 +357,7 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("COMMUNICATION ERROR"),
         // r=5, c=20
-        N_("MMU unit not responding correctly. Check the wiring and connectors. If the issue persists contact support."),
+        N_("MMU unit not responding correctly. Check the wiring and connectors. If the issue persists, contact support."),
         ERR_CONNECT_COMMUNICATION_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
@@ -359,7 +376,7 @@ static constexpr MMUErrorDesc error_list[] = {
     // r=1, c=20
     { N_("INVALID TOOL"),
         // r=5, c=20
-        N_("Requested filament tool is not available on this hardware. Check the G-code file for possible issue."),
+        N_("Requested filament tool is not available on this hardware. Check the G-code for tool index out of range (T0-T4)."),
         ERR_SYSTEM_INVALID_TOOL, 
         { ButtonOperations::StopPrint, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
     },
@@ -376,23 +393,23 @@ static constexpr MMUErrorDesc error_list[] = {
     { N_("FW UPDATE NEEDED"),
         // r=5, c=20
         N_("The MMU unit reports its FW version incompatible with the printer's firmware. Make sure the MMU firmware is up to date."),
-        ERR_SYSTEM_VERSION_MISMATCH, 
+        ERR_SYSTEM_FW_UPDATE_NEEDED, 
       { ButtonOperations::NoOperation, ButtonOperations::DisableMMU, ButtonOperations::NoOperation }
     },
 
     // r=1, c=20
     { N_("FW RUNTIME ERROR"),
         // r=5, c=20
-        N_("Internal runtime error. Try resetting the MMU unit or updating the firmware. If the issue persists contact support."),
-        ERR_SYSTEM_RUNTIME_ERROR,
+        N_("Internal runtime error. Try resetting the MMU unit or updating the firmware. If the issue persists, contact support."),
+        ERR_SYSTEM_FW_RUNTIME_ERROR,
       { ButtonOperations::NoOperation, ButtonOperations::ResetMMU, ButtonOperations::NoOperation }
-    }
+    },
     
     // r=1, c=20
     { N_("UNLOAD MANUALLY"),
         // r=5, c=20
         N_("Unexpected FINDA reading. Ensure no filament is under FINDA and the selector is free. Check FINDA connection."),
-        ERR_SYSTEM_RUNTIME_ERROR,
+        ERR_SYSTEM_FW_RUNTIME_ERROR,
         { ButtonOperations::NoOperation, ButtonOperations::Retry, ButtonOperations::NoOperation }
     }
 };
